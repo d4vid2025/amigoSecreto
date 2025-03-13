6250f2ebb645ffcf7c0c1f7113d7ec80199b98c5
stevenAnto <ecalcinap@unsa.edu.pe>
2025-03-13 17:19:20 -0500

    fucnion agregar amigos y actualizar implementadas

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación.
+//  Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos  = [];
+console.log(amigos);
+
+function agregarAmigo(){
+    let elementoAmigoNuevo = document.querySelector("#amigo");
+    let amigoNuevo = elementoAmigoNuevo.value;
+    if(amigoNuevo==''){
+        alert("Tienes que escribir algo en el campo de amigos");
+    }else{
+        amigos.push(amigoNuevo);
+        console.log(amigos);
+    }
+    elementoAmigoNuevo.value ='';
+    actualizarLista();
+}
+function actualizarLista(){
+    let listaAmigos = document.querySelector("#listaAmigos");
+    listaAmigos.innerHTML = amigos.map(amigo =>`<li>${amigo}</li>`).join("");
+}
